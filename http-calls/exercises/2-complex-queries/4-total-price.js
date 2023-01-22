4624437e6fcd0886cad0e0c8f0c18db7f4773c36
galalkoro98 <galal.koro@gmail.com>
2023-01-22 22:27:38 +0100

    exercise done

--- http-calls/exercises/2-complex-queries/4-total-price.js
+++ http-calls/exercises/2-complex-queries/4-total-price.js
@@ -1,24 +1,80 @@
 /**
- * Fill in the blanks to create a script 
- * that prints the total cost if someone would buy one of every item 
+ * Fill in the blanks to create a script
+ * that prints the total cost if someone would buy one of every item
  * (ignoring whether it is in stock or not and not applying any discount)
  */
 
- import "./qs.js";
- async function ex4() {
-   const query = qs.stringify(
-   {
-       _
-   }, 
-   {
-     encodeValuesOnly: true,
-   });
-   console.log("The query string", query);
- 
-   // call the matching endpoint and include the querystring after the ?
-   const baseUrl = _;
-   const response = await fetch(`${_}?${query}`);
-   const result = await response.json();
-   _
- }
- ex4();
+import "./qs.js";
+async function ex4() {
+  const query = qs.stringify(
+    {
+      // that prints the total cost if someone would buy one of every item
+
+      fields: ["price"],
+      sort: ["price:asc"],
+      filter: {
+        price: {
+          gt: 0,
+        },
+        aggregate: {
+          total: {
+            sum: "price",
+          },
+
+          groupBy: ["price"],
+
+          page: {
+            limit: 100,
+          },
+
+          // ignoring whether it is in stock or not and not applying any discount
+          filter: {
+            price: {
+              gt: 0,
+            },
+
+            outOfStock: {
+              eq: false,
+            },
+
+            discount: {
+              eq: null,
+            },
+
+            aggregate: {
+              total: {
+                sum: "price",
+              },
+            },
+
+            page: {
+              limit: 100,
+            },
+
+            groupBy: ["price"],
+            sort: ["price:asc"],
+          },
+        },
+      },
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  console.log("The query string", query);
+
+  // call the matching endpoint and include the querystring after the ?
+  const baseUrl = "http://localhost:1337/api/products";
+  const response = await fetch(`${baseUrl}?${query}`);
+  const result = await response.json();
+
+  // calculate the total cost
+  const totalCost = result.data.reduce((acc, item) => {
+    return acc + item.attributes.price;
+  });
+
+  // print the total cost
+
+  console.log("The total cost is", totalCost);
+}
+ex4();

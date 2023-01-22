e3de808a88f289043f47284449887a5e69fcd2d4
galalkoro98 <galal.koro@gmail.com>
2023-01-22 22:27:10 +0100

    exercise done

--- http-calls/exercises/2-complex-queries/3-name-search.js
+++ http-calls/exercises/2-complex-queries/3-name-search.js
@@ -1,31 +1,37 @@
 /**
- * Fill in the blanks to create a script 
+ * Fill in the blanks to create a script
  * searches for products whose name contains the string that is passed as nameStr
  */
 
- import "./qs.js";
- async function searchProductByName(nameStr) {
-   const query = qs.stringify(
-   {
-       _
-   }, 
-   {
-     encodeValuesOnly: true,
-   });
-   console.log("The query string", query);
- 
-   // call the matching endpoint and include the querystring after the ?
-   const baseUrl = _;
-   const response = await fetch(`${_}?${query}`);
-   const result = await response.json();
-   _
- }
+import "./qs.js";
+async function searchProductByName(nameStr) {
+  const query = qs.stringify(
+    {
+      fields: ["name"],
+      sort: ["name:asc"],
+      filter: {
+        name: {
+          contains: nameStr,
+        },
+      },
+    },
 
- async function test() {
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  console.log("The query string", query);
+
+  const baseUrl = "http://localhost:1337/api/products";
+  const response = await fetch(`${baseUrl}?${query}`);
+  const result = await response.json();
+  return result.data.map((product) => product.attributes.name);
+}
+
+async function test() {
   console.log("Products containing name", await searchProductByName("name"));
   console.log("Products containing prog", await searchProductByName("prog"));
   console.log("Products containing pro", await searchProductByName("pro"));
- }
+}
 
- test();
-
+test();

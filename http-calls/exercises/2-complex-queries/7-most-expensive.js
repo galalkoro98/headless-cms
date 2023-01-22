dbcc585e881d168be7c665d74dec71c8606ce06a
galalkoro98 <galal.koro@gmail.com>
2023-01-22 22:28:31 +0100

    exercise done

--- http-calls/exercises/2-complex-queries/7-most-expensive.js
+++ http-calls/exercises/2-complex-queries/7-most-expensive.js
@@ -1,32 +1,71 @@
 /**
- * Fill in the blanks to create a script 
+ * Fill in the blanks to create a script
  * that prints the name of the most expensive item that someone can buy (after applying any discounts,
  * and not including any items that are out of stock)
  */
 
- import "./qs.js";
- async function searchProductByName(nameStr) {
-   const query = qs.stringify(
-   {
-       _
-   }, 
-   {
-     encodeValuesOnly: true,
-   });
-   console.log("The query string", query);
- 
-   // call the matching endpoint and include the querystring after the ?
-   const baseUrl = _;
-   const response = await fetch(`${_}?${query}`);
-   const result = await response.json();
-   _
- }
+import "./qs.js";
+async function searchProductByName(nameStr) {
+  const query = qs.stringify(
+    {
+      _where: [
+        {
+          name_contains: nameStr,
+        },
 
- async function test() {
+        {
+          outOfStock: false,
+        },
+
+        {
+          discount: null,
+        },
+
+        {
+          price: {
+            _gte: 0,
+          },
+
+          outOfStock: false,
+
+          discount: null,
+
+          name_contains: nameStr,
+
+          _sort: "price:desc",
+
+          _limit: 1,
+
+          _start: 0,
+        },
+      ],
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  console.log("The query string", query);
+
+  const baseUrl = "http://localhost:1337/api/products";
+  const response = await fetch(`${baseUrl}?${query}`);
+  const result = await response.json();
+
+  // Extract the name of the most expensive item
+  if (result.data.length > 0) {
+    const mostExpensive = result.data[0];
+    console.log(
+      "The name of the most expensive item: " + mostExpensive.attributes.name
+    );
+    return mostExpensive.attributes.name;
+  } else {
+    console.log("No Product found");
+  }
+}
+
+async function test() {
   console.log("Products containing name", await searchProductByName("name"));
   console.log("Products containing prog", await searchProductByName("prog"));
   console.log("Products containing pro", await searchProductByName("pro"));
- }
+}
 
- test();
-
+test();

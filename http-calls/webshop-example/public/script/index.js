a1811bf40713a1a02ddfe6a5bcff523d9b78d11c
galalkoro98 <galal.koro@gmail.com>
2023-01-28 21:21:37 +0100

    update

--- http-calls/webshop-example/public/script/index.js
+++ http-calls/webshop-example/public/script/index.js
@@ -12,31 +12,37 @@ async function createItemElement(itemData) {
   itemEl.appendChild(titleEl);
 
   const priceEl = document.createElement("div");
-  priceEl.className = "price"; 
-  if(itemData.outOfStock) {
-    priceEl.innerHTML = '<div class="outOfStock">Out of stock</div>'; 
+  priceEl.className = "price";
+  if (itemData.outOfStock) {
+    priceEl.innerHTML = '<div class="outOfStock">Out of stock</div>';
   } else {
     if (itemData.discount.data) {
       const discountAmount = itemData.discount.data.attributes.percentage;
       priceEl.innerHTML = `
             <div class="discount">${discountAmount}%</div>
             <div class="oldPrice">${itemData.price.toFixed(2)}€</div>
-            <div class="newPrice">${(itemData.price * (1 - (discountAmount/100))).toFixed(2)}€</div>
+            <div class="newPrice">${(
+              itemData.price *
+              (1 - discountAmount / 100)
+            ).toFixed(2)}€</div>
       `;
-    }else {
+    } else {
       const newPrice = document.createElement("div");
       newPrice.className = "newPrice";
       newPrice.innerText = itemData.price.toFixed(2) + "€";
       priceEl.appendChild(newPrice);
     }
-  } 
+  }
   itemEl.appendChild(priceEl);
 
   const descriptionEl = document.createElement("div");
   descriptionEl.className = "itemDescription";
-  descriptionEl.innerText = itemData.description.length > 100 ? itemData.description.substring(1, 100) + '...': itemData.description;
+  descriptionEl.innerText =
+    itemData.description.length > 100
+      ? itemData.description.substring(1, 100) + "..."
+      : itemData.description;
   itemEl.appendChild(descriptionEl);
-  
+
   return itemEl;
 }
 
@@ -56,7 +62,7 @@ async function createCategoryContainer(entry) {
 
   const itemsContainer = document.createElement("div");
   itemsContainer.className = "items";
-  entry.attributes.products.data.forEach(async itemEntry => {
+  entry.attributes.products.data.forEach(async (itemEntry) => {
     const itemEl = await createItemElement(itemEntry.attributes);
     itemEl.className = "item";
     itemsContainer.appendChild(itemEl);
@@ -66,18 +72,23 @@ async function createCategoryContainer(entry) {
   return el;
 }
 
-async function loadProducts() {
-  const response = await (await fetch("/api/categories?populate[products][populate][0]=discount&populate[products][populate][1]=image", {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer afaddde6fe34f2b035219d46d1b562bb14a0ec5a5c7e54b5a7bb48f8e32c1aef7427e4254f0a941476620095c249874dcfe68ef34ef1f4d1f1f1080ba6a0116868f9d1ca4f3de9ef172569653e6ddb2a0cae35c3dded89e679b8f28cb1b22b903866a1c2dc484cd47d3acead3ce7a92838f0e032d19586ed716c2e1b4e224ccc`
-    }
-  })).json();
-  const body = document.getElementById("body");
-  body.innerHTML = "";
-  response.data.forEach(async entry => {
-    const categoryContainer = await createCategoryContainer(entry); 
-    body.appendChild(categoryContainer);
+async function loadProducts() {
+  const response = await (
+    await fetch(
+      "/api/categories?populate[products][populate][0]=discount&populate[products][populate][1]=image",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer afaddde6fe34f2b035219d46d1b562bb14a0ec5a5c7e54b5a7bb48f8e32c1aef7427e4254f0a941476620095c249874dcfe68ef34ef1f4d1f1f1080ba6a0116868f9d1ca4f3de9ef172569653e6ddb2a0cae35c3dded89e679b8f28cb1b22b903866a1c2dc484cd47d3acead3ce7a92838f0e032d19586ed716c2e1b4e224ccc`,
+        },
+      }
+    )
+  ).json();
+  const main = document.getElementById("main");
+  main.innerHTML = "";
+  response.data.forEach(async (entry) => {
+    const categoryContainer = await createCategoryContainer(entry);
+    main.appendChild(categoryContainer);
   });
 }
 
